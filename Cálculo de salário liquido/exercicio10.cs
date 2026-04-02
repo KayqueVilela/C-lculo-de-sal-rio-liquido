a318b2def69986b99cc2fe4b464d8b86005bc783
Mona Lisa <edusantos160506@gmail.com>
2026-04-02 11:54:42 -0300

    .

---
+++ Cálculo de salário liquido/exercicio10.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cálculo_de_salário_liquido
+{
+    public partial class exercicio10 : Form
+    {
+        public exercicio10()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float comp;
+            comp = float.Parse(textBox1.Text);
+            float larg;
+            larg = float.Parse(textBox2.Text);
+            float alt;
+            alt = float.Parse(textBox3.Text);
+            float vol;
+            vol = comp * larg * alt;
+            label5.Text = "O volume da caixa é: " + vol;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            label5.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
